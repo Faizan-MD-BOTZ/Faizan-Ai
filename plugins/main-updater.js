310dfae504db0262afb541bd89cca9786e631cb3
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-11-18 00:52:18 +0500

    Add files via upload

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for FAIZAN-AI updates...");
+        await reply("🔍 Checking for Arslan-MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://github.com/Faizan-MD-BOTZ/Faizan-Ai");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/QadeerXTech/REDZONE-MD/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your FAIZAN-AI bot is already up-to-date!");
+            return reply("✅ Your REDZONE-MD bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating 𝐅𝐀𝐈𝐙𝐀𝐍-𝐀𝐈 𝐁𝐎𝐓...");
+        await reply("🚀 Updating REDZONE-MD Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/Faizan-MD-BOTZ/Faizan-Ai", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/QadeerXTech/REDZONE-MD/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "/FAIZAN-AI-main");
+        const sourcePath = path.join(extractPath, "/REDZONE-MD-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
