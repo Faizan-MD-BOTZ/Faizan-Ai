a71b73faa1be3e523f2844e276570029c1918105
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-11-18 00:27:53 +0500

    Add files via upload

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -27,7 +27,7 @@ cmd({
     }
 
     // Fetch latest version data from GitHub
-    const rawVersionUrl = 'https://raw.githubusercontent.com/Arslan-MD/Arslan_MD/main/data/version.json';
+    const rawVersionUrl = 'https://raw.githubusercontent.com/ADEEL967MD/ADEEL-MD/main/data/version.json';
     let latestVersion = 'Unknown';
     let latestChangelog = 'No changelog available.';
     try {
@@ -53,12 +53,12 @@ cmd({
     const lastUpdate = fs.statSync(localVersionPath).mtime.toLocaleString();
 
     // GitHub stats
-    const githubRepo = 'https://github.com/ANAYAT-AI/ANAYAT-AI/tree/main';
+    const githubRepo = 'https://github.com/ADEEL967MD/ADEEL-MD';
 
     // Check update status
-    let updateMessage = `✅ Your Anayat-ai bot is up-to-date!`;
+    let updateMessage = `✅ Your REDZONE-MD bot is up-to-date!`;
     if (localVersion !== latestVersion) {
-      updateMessage = `🚀 Your Anayat-ai bot is outdated!
+      updateMessage = `🚀 Your ADEEL-MD bot is outdated!
 🔹 *Current Version:* ${localVersion}
 🔹 *Latest Version:* ${latestVersion}
 
@@ -66,22 +66,22 @@ Use *.update* to update.`;
     }
 
     const statusMessage = `🌟 *Good ${new Date().getHours() < 12 ? 'Morning' : 'Night'}, ${pushname}!* 🌟\n\n` +
-      `📌 *Bot Name:* ArslanMD-MD\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
+      `📌 *Bot Name:* ADEEL-MD\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
       `💾 *System Info:*\n⏳ *Uptime:* ${uptime}\n📟 *RAM Usage:* ${ramUsage}MB / ${totalRam}MB\n⚙️ *Host Name:* ${hostName}\n📅 *Last Update:* ${lastUpdate}\n\n` +
       `📝 *Changelog:*\n${latestChangelog}\n\n` +
-      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈](https://github.com/ANAYAT-AI/ANAYAT-AI/tree/main)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
+      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [𝐀𝐃𝐄𝐄𝐋-𝐌𝐃](https://github.com/ADEEL967MD)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
 
     // Send the status message with an image
     await conn.sendMessage(from, {
-      image: { url: 'https://files.catbox.moe/rexuf8.jpg' },
+      image: { url: 'https://files.catbox.moe/hg5hj6.jpg' },
       caption: statusMessage,
       contextInfo: {
         mentionedJid: [m.sender],
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363420933039839@newsletter',
-          newsletterName: '𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈',
+          newsletterJid: '120363403380688821@newsletter',
+          newsletterName: '𝐀𝐃𝐄𝐄𝐋-𝐌𝐃',
           serverMessageId: 143
         }
       }
